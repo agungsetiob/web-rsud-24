95841128cbf49278c8db805e0472d8306f1097f4
Agung Setio Budi <agungsetiobudi19@gmail.com>
2024-07-30 15:25:18 +0800

    add title

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>{{$title}}</title>
+        <title>{{ config('app.title') }}</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="rsud tanah bumbu" name="keywords">
         <meta content="rumah sakit umum daerah tanah bumbu amanah husada" name="description">
