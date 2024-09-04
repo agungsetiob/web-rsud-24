8141f896c8c3c0a3ce57bff2e961317968614185
Agung Setio Budi <agungsetiobudi19@gmail.com>
2024-09-04 21:46:38 +0800

    add menu

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RSUD DHAAN</title>
+    <title>SIPLAYER - RSUD</title>
     <link rel="shortcut icon" href="https://rsud.tanahbumbukab.go.id/img/logo.png" type="image/x-icon"/>
     <style>
         body {
@@ -12,18 +12,22 @@
             margin: 0;
             padding: 0;
             display: flex;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
             min-height: 100vh;
             box-sizing: border-box;
         }
+
         .container {
             max-width: 1000px;
             display: grid;
-            grid-template-columns: repeat(3, 1fr);
+            grid-template-columns: repeat(2, 1fr); /* 2 cards per row */
             grid-gap: 20px;
             padding: 20px;
+            width: 100%;
         }
+
         .card {
             background-color: white;
             border-radius: 15px;
@@ -35,23 +39,27 @@
             justify-content: center;
             text-align: center;
             position: relative;
-            transition: transform 0.3s ease, background-color 0.3s ease;
+            transition: transform 0.3s ease, background-image 0.7s ease;
         }
+
         .card a {
             text-decoration: none;
         }
+
         .card img {
             width: 50px;
             height: 50px;
             transition: width 0.3s, height 0.3s; /* Smooth transition for resizing */
         }
+
         .card h3 {
             margin: 15px 0 0;
             font-size: 18px;
             color: #4A00E0;
         }
+
         .logo {
-            grid-column: span 3;
+            grid-column: span 2;
             text-align: left;
             font-size: 29px;
             color: #FFEB3B;
@@ -61,6 +69,7 @@
         /* Effect when hovering */
         .card:hover {
             background-color: #f0f0f0;
+            background-image: linear-gradient(#0074e5, #ff8a00);
             transform: scale(1.05); /* Slight zoom effect */
         }
 
@@ -68,7 +77,7 @@
         @media (min-width: 1024px) {
             .container {
                 max-width: 1200px;
-                grid-template-columns: repeat(3, 1fr);
+                grid-template-columns: repeat(2, 1fr); /* 2 cards per row */
             }
             .card {
                 padding: 30px;
@@ -81,7 +90,7 @@
 
         @media (max-width: 768px) {
             .container {
-                grid-template-columns: repeat(2, 1fr);
+                grid-template-columns: repeat(2, 1fr); /* 2 cards per row for tablets */
             }
             .logo {
                 grid-column: span 2;
@@ -90,7 +99,7 @@
 
         @media (max-width: 480px) {
             .container {
-                grid-template-columns: 1fr;
+                grid-template-columns: 1fr; /* Single column for small screens */
             }
             .logo {
                 grid-column: span 1;
@@ -100,6 +109,21 @@
                 font-size: 16px;
             }
         }
+
+        footer {
+            color: white;
+            padding: 15px 20px;
+            text-align: center;
+            font-size: 14px;
+            width: 100%;
+            position: relative;
+            bottom: 0;
+        }
+
+        .disabled {
+            cursor: not-allowed;
+        }
+
     </style>
 </head>
 <body>
@@ -119,10 +143,35 @@
         </div>
         <div class="card">
             <a href="https://simgos.tanahbumbukab.go.id/apps/RegOnline" target="_blank">
-                <img src="{{asset('img/pentol.png')}}" alt="Antrian Online Icon">
+                <img src="{{asset('img/pamanpentol.png')}}" alt="Antrian Online Icon">
                 <h3>PAMANPENTOL (Aplikasi Manajemen Antrian dan Pendaftaran Online)</h3>
             </a>
         </div>
+        <div class="card disabled">
+            <a class="disabled" href="javascript:void(0)">
+                <img src="{{asset('img/palkon.png')}}" alt="Palkon Online Icon">
+                <h3>SIPALKON (Sistem Pengingat Jadwal Kontrol)</h3>
+            </a>
+        </div>
+        <div class="card disabled">
+            <a class="disabled" href="javascript:void(0)">
+                <img src="{{asset('img/supriadi.svg')}}" alt="Supriadi Online Icon">
+                <h3>e-SUPRIADI (Surat Perintah Perjalanan Dinas Online)</h3>
+            </a>
+        </div>
+        <div class="card disabled">
+            <a class="disabled" href="javascript:void(0)">
+                <h3 style="text-align: left; color: black;">Tentang Sistem</h3>
+                <p style="text-align: justify; color: black;">
+                    Sistem Integrasi Pelayanan dan Administrasi Elektronik (SIPLAYER) 
+                    merupakan sebuah inovasi dari RSUD dr. H. Andi Abdurrahman Noor demi
+                    mewujudkan pelayanan yang lebih baik untuk masyarakat.
+                </p>
+            </a>
+        </div>
     </div>
+    <footer>
+        <p>&copy; 2024 Agung Setio Budi</p>
+    </footer>
 </body>
 </html>
