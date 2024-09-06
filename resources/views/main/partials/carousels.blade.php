fad86ffc6dac338e43301fdb25c81ba256c1c5dd
agungsetiob <agungsetiobudi19@gmail.com>
2024-09-06 16:07:25 +0800

    add budaya pelayanan

--- resources/views/main/partials/carousels.blade.php
+++ resources/views/main/partials/carousels.blade.php
@@ -13,9 +13,8 @@
         <img src="{{asset('img/carousel-2.jpg')}}" class="img-fluid w-100" alt="Image">
         <div class="carousel-caption">
             <div class="carousel-caption-content p-3">
-                <h5 class="text-white text-uppercase fw-bold mb-4" style="letter-spacing: 3px;">RSUD dr. H. Andi Abdurrahman Noor
-                    </h5>
-                <h1 class="display-1 text-capitalize text-white mb-4">Senyum Santun Sapa</h1>
+                <h5 class="text-white text-uppercase fw-bold mb-4" style="letter-spacing: 3px;">RSUD dr. H. Andi Abdurrahman Noor</h5>
+                <!-- <h1 class="display-1 text-capitalize text-white mb-4">Senyum Santun Sapa</h1> -->
             </div>
         </div>
     </div>
