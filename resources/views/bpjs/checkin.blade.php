4bb8b8d740d08f56a83c24a6b055312840cc6ee5
Agung Setio Budi <agungsetiobudi19@gmail.com>
2024-12-07 10:34:21 +0800

    push

--- resources/views/bpjs/checkin.blade.php
+++ resources/views/bpjs/checkin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BPJS Check-In</title>
-    <link rel="shortcut icon" href="{{asset('logors.png')}}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{url ('storage/logors.png')}}" type="image/x-icon" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
