7808986ecfe5a4b98280350c3bb920a913271494
Agung Setio Budi <agungsetiobudi19@gmail.com>
2024-12-26 20:59:18 +0800

    custom 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,26 @@
-@extends('errors::minimal')
+{{--@extends('errors::minimal')
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', __('Service Unavailable'))
+@section('message', __('Service Unavailable'))--}}
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link
+      rel="shortcut icon"
+      href="{{url ('storage/logors.png')}}"
+      type="image/x-icon"
+    />
+    <title>503 - Service Unavailable - Under Maintainance</title>
+    <link href="{{ asset('css/error.css') }}" rel="stylesheet" type="text/css"  media="all" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Bangers">
+</head>
+<body class="error-page">
+    <div class="content">
+        <img src="{{ url('storage/hulk-404.gif') }}" title="error" />
+        <p><span></span>Under Maintainance</p>
+        {{--<a id="hulk-button" href="{{ url('/') }}">Back To Home</a>--}}
+    </div>
+</body>
+</html>
